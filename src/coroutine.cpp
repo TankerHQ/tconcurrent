910a75fdace93db8dfd5161bc380daef2d68b978
Philippe <philippe@tanker.io>
2016-04-22 11:58:45 +0200

    Fix build on osx

--- src/coroutine.cpp
+++ src/coroutine.cpp
@@ -1,17 +1,22 @@
 #include <tconcurrent/coroutine.hpp>
 
+#include <boost/thread.hpp>
+
 namespace tconcurrent
 {
 namespace detail
 {
 namespace
 {
-thread_local detail::coroutine_control* current;
+boost::thread_specific_ptr<detail::coroutine_control*> current;
 }
 
 detail::coroutine_control*& get_current_coroutine_ptr()
 {
-  return current;
+  auto p = current.get();
+  if (!p)
+    current.reset(p = new detail::coroutine_control*(nullptr));
+  return *p;
 }
 
 }
