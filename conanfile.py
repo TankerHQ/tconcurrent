dbe517b54c75622992ea3506a8f53e4d48848245
Théo DELRIEU <theo.delrieu@tanker.io>
2019-10-15 18:10:04 +0200

    Revert "fix mingw-threads version"
    This reverts commit f443a8a19e448fb2965dc0ae4268e7935ce00725.

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,7 @@ class TconcurrentConan(ConanFile):
         self.requires("Boost/1.71.0@tanker/testing")
         self.requires("enum-flags/0.1a@tanker/testing")
         if self.is_mingw:
-            self.requires("mingw-threads/1.0.0@tanker/testing")
+            self.requires("mingw-threads/86ca7d@tanker/testing")
 
     def build_requirements(self):
         if self.should_build_tests:
