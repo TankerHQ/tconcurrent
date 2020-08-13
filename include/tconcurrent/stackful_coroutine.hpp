b52c09ce1cfbf1b87060fb5c04d0f67aac9db77c
Philippe Daouadi <philippe.daouadi@tanker.io>
2020-08-13 11:51:16 +0200

    stackful_coroutine: remove useless capture

--- include/tconcurrent/stackful_coroutine.hpp
+++ include/tconcurrent/stackful_coroutine.hpp
@@ -280,8 +280,7 @@ typename std::decay_t<Awaitable>::value_type coroutine_control::await(
 
     coroutine_exit_post_setup = [&aborted,
                                  &progressing_awaitable,
-                                 &finished_awaitable,
-                                 &awaitable](coroutine_control* ctrl) {
+                                 &finished_awaitable](coroutine_control* ctrl) {
       progressing_awaitable.then(
           ctrl->executor_,
           [aborted, &finished_awaitable, ctrl](std::decay_t<Awaitable> f) {
