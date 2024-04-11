bd9b72d5b1501bc10eb9494b831de92ae2031a32
Timothée Isnard <timothee.isnard@tanker.io>
2024-04-11 10:23:51 +0200

    maint: Update run-ci and gitlab-ci for new tankerci API

--- run-ci.py
+++ run-ci.py
@@ -7,6 +7,7 @@ import tankerci
 import tankerci.conan
 import tankerci.cpp
 import tankerci.git
+from tankerci.conan import Profile
 
 
 def main() -> None:
@@ -20,12 +21,21 @@ def main() -> None:
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
 
     build_and_test_parser = subparsers.add_parser("build-and-test")
-    build_and_test_parser.add_argument("--profile", required=True)
     build_and_test_parser.add_argument("--coverage", action="store_true")
+    build_and_test_parser.add_argument("--remote", default="artifactory")
+
+    build_and_test_parser.add_argument(
+        "--profile",
+        dest="profiles",
+        nargs="+",
+        type=str,
+        required=True,
+    )
 
     args = parser.parse_args()
+    user_home = None
     if args.home_isolation:
-        tankerci.conan.set_home_isolation()
+        user_home = Path.cwd() / ".cache" / "conan" / args.remote
 
     tankerci.conan.update_config()
 
@@ -46,8 +56,17 @@ def main() -> None:
             # When a macOS runner runs the tests, the ones waiting for a specific time will wait longer than requested.
             # Thus the tests fail. Funny thing is that they pass when running them by hand, on the slave...
             ctest_flags.append("--test-case-exclude=*[waiting]*")
-        built_path = tankerci.cpp.build(args.profile, coverage=args.coverage)
-        tankerci.cpp.check(built_path, coverage=args.coverage, ctest_flags=ctest_flags)
+
+        with tankerci.conan.ConanContextManager(
+            [args.remote, "conancenter"], conan_home=user_home
+        ):
+            built_path = tankerci.cpp.build(
+                    host_profile=Profile(args.profiles),
+                    build_profile=tankerci.conan.get_build_profile(),
+                    coverage=args.coverage,
+                    make_package=True,
+                    )
+            tankerci.cpp.check(built_path, coverage=args.coverage, ctest_flags=ctest_flags)
     else:
         parser.print_help()
         sys.exit(1)
